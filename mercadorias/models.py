93ee7572ae538ca2f431d108ddee74ae0be127a5
NisioH <nisiohelandeoliveira@gmil.com>
2025-06-11 10:44:09 -0400

    Primeiros arquivos

---
+++ mercadorias/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Setor(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Mercadoria(models.Model):
+    nome = models.CharField(max_length=100)
+    quantidade = models.IntegerField()
+    setor = models.ForeignKey(Setor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
